f2f2210f81c45aea0af37c6584bfa9056f384d92
krote5k <krotesk@mail.ru>
2021-07-09 11:01:33 +0500

    Поправка файлов и ридми

--- blocks/bd.php
+++ blocks/bd.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysql_connect("localhost","asteriskuser","+ltyd9IpCIDMyZ3Bs");
+$db = mysql_connect("localhost","asteriskuser","asteriskpass");
 mysql_select_db("asterisk",$db);
 mysql_query("set names utf8",$db);
 ?>
--- blocks/values.php
+++ blocks/values.php
@@ -1,13 +1,16 @@
 <?php
         //Глобальные параметры
         $exten = 1955;
+        //Настройки для офиса
         $sip_serv_office = '192.168.55.250';
         $ntp_serv_office = 'ntp3.vniiftri.ru';
+        //Настройки для VPN
         $sip_serv_magazin = '192.168.100.250';
         $ntp_serv_magazin = 'ntp3.vniiftri.ru';
-        $sip_serv_overnat = '85.192.100.250';
+        // Настройки при подключении за NAT
+        $sip_serv_overnat = '85.192.*.250';
         $ntp_serv_overnat = 'ntp3.vniiftri.ru';
-        //Каталог конфигов
+        //Каталог конфигов (веб-каталог)
         $filepath = "/var/www/localhost/htdocs/pbx/";
         //Каталог TFTP
         $tftp_path = "/tftpboot/";
